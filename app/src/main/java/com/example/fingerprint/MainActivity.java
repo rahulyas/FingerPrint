2a8a6a1ffea046d8115d6247f0f270d693255e5f
Rahul <rahulyash30@gmail.com>
2022-08-23 19:05:36 +0530

    Initial commit

---
+++ app/src/main/java/com/example/fingerprint/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.fingerprint;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.biometric.BiometricManager;
+import androidx.biometric.BiometricPrompt;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.content.ContextCompat;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.concurrent.Executor;
+
+public class MainActivity extends AppCompatActivity {
+
+    BiometricPrompt biomerticPrompt;
+    BiometricPrompt.PromptInfo promptInfo;
+    ConstraintLayout constraintLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        constraintLayout = findViewById(R.id.main_id);
+        BiometricManager biometricManager = BiometricManager.from(this);
+        switch (biometricManager.canAuthenticate())
+        {
+            case BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE:
+                Toast.makeText(this, "Device Does have not fingerPrint", Toast.LENGTH_SHORT).show();
+                break;
+
+            case BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE:
+                Toast.makeText(this, "Not Working", Toast.LENGTH_SHORT).show();
+
+            case BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED:
+                Toast.makeText(this, "No FingerPrint Assigned", Toast.LENGTH_SHORT).show();
+        }
+
+        Executor executor = ContextCompat.getMainExecutor(this);
+
+        biomerticPrompt = new BiometricPrompt(MainActivity.this, executor, new BiometricPrompt.AuthenticationCallback() {
+            @Override
+            public void onAuthenticationError(int errorCode, @NonNull CharSequence errString) {
+                super.onAuthenticationError(errorCode, errString);
+            }
+
+            @Override
+            public void onAuthenticationSucceeded(@NonNull BiometricPrompt.AuthenticationResult result) {
+                Toast.makeText(MainActivity.this, "Login Sucess", Toast.LENGTH_SHORT).show();
+                constraintLayout.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onAuthenticationFailed() {
+                super.onAuthenticationFailed();
+            }
+        });
+
+        promptInfo = new BiometricPrompt.PromptInfo.Builder().setTitle("Rahul")
+                .setDescription("User FingerPrint To Login").setDeviceCredentialAllowed(true).build();
+
+        biomerticPrompt.authenticate(promptInfo);
+
+    }
+}
